e1298ac82e341ddfdd44f4bdac210f721a0f5bad
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2020-06-30 13:13:43 +0530

    Added Headers data class

--- src/main/kotlin/http/Http.kt
+++ src/main/kotlin/http/Http.kt
@@ -1,3 +1,25 @@
 package http
 
-data class Header(val name: String, val value: String)
+import enums.HttpHeader
+import enums.HttpMethod
+import enums.HttpVersion
+import java.nio.file.Path
+import java.util.*
+import kotlin.collections.HashMap
+
+data class Headers(val map: EnumMap<HttpHeader, String>) {
+    fun appendToHeader(key: HttpHeader, value: String) {
+        val v = map[key]
+
+        if (v == null)
+            map[key] = value
+        else
+            map[key] = "$v, $value"
+    }
+
+    fun getHeaderValue(key: HttpHeader): String? {
+        return map[key]
+    }
+}
+
+data class StatusLine(val httpMethod: HttpMethod, val path: String, val httpVersion: HttpVersion)
